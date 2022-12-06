7b4c6557f66444940b067c939e1907fdb9c922ae
Tyler <53319122+CipherOps@users.noreply.github.com>
2022-12-06 18:41:15 -0500

    Update PiholeUninstall.sh

--- PiholeUninstall.sh
+++ PiholeUninstall.sh
@@ -15,35 +15,41 @@ if [ "$confirm" != "C" ]; then
 fi
   # uninstall pihole
   sudo pihole uninstall
-
+  echo "Pihole uninstalled, removed Pihole directory"
   # remove pihole directory
   sudo rm -rf /etc/pihole/
-
+  echo "Pihole directory removed, restoring network configuraiton"
 # check if /etc/resolv.conf.orig exists
 if [ -f "/etc/resolv.conf.orig" ]; then
     mv /etc/resolv.conf.orig /etc/resolv.conf
+    echo "Original resolv.conf moved to /etc/resolv.conf"
 else
     # create a default /etc/resolv.conf file with 9.9.9.9 as the DNS
     echo "nameserver 9.9.9.9" > /etc/resolv.conf
+    echo "New resolv.conf created"
 fi
 
 # check if systemd-resolvd is installed and enable it
 if [ -f "/usr/lib/systemd/resolv.conf" ]; then
     ln -sf /usr/lib/systemd/resolv.conf /etc/resolv.conf
     systemctl enable --now systemd-resolved
+    echo "systemd-resolved is re-enabled"
 fi
 
   # check if system uses Network Manager or wicked
   if [ -f /etc/NetworkManager/NetworkManager.conf ]; then
     # restart Network Manager service
     sudo systemctl restart NetworkManager
+    echo "NetworkManager restarted to apply DNS config change"
   elif [ -f /etc/sysconfig/network/config ]; then
     # restart wickd service
     sudo systemctl restart wicked
+    echo "wickd network manager restarted to apply DNS config change"
   fi
 
   # check if system uses netplan
   if [ -d /etc/netplan/ ]; then
+  echo "Netplan detetcted, installed default netplan.yaml"
     # create basic netplan configuration
     sudo touch /etc/netplan/00-netplan.yaml
     sudo cat <<'EOT' >> /etc/netplan/00-netplan.yaml
@@ -62,4 +68,5 @@ EOT
     # unable to determine how to restart networking service
     echo "Error: unable to determine how to restart networking service. Please restart it manually."
   fi
+  echo "Pihole is uninstalled"
 done
